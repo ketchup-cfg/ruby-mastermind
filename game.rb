ee8a85a41d3a331f118507ce4c463578b68e38d4
Trevor Pierce <yeahtrevorpierce@gmail.com>
2024-03-29 09:00:14 -0500

    Get user guess and validate

--- game.rb
+++ game.rb
@@ -7,6 +7,8 @@ require_relative 'peg'
 class Game
   attr_reader :board, :code
 
+  VALID_COLORS = %w[r g b y w m].freeze
+
   def initialize
     reset
   end
@@ -18,12 +20,39 @@ class Game
 
   def set_code
     4.times do
-      @code << [Peg.green, Peg.red, Peg.blue, Peg.yellow, Peg.white].sample
+      @code << [Peg.green, Peg.red, Peg.blue, Peg.yellow, Peg.white, Peg.magenta].sample
     end
+  end
+
+  def guesses_valid?(guesses)
+    if guesses.length != 4
+      puts 'Please only enter 4 colors, seperated by spaces'
+      false
+    elsif guesses.any? { |guess| !VALID_COLORS.include?(guess) }
+      puts 'Please only enter valid colors'
+      puts 'Valid colors are: (r)ed, (g)reen, (b)lue, (y)ellow, (w)hite), (m)agenta'
+      false
+    else
+      true
+    end
+  end
+
+  def guess
+    guesses = []
+    valid = false
+
+    until valid
+      print 'Please enter your guess, with each color seperated by spaces: '
+      guesses = gets.chomp.downcase.split.map { |guess| guess[0] }
+
+      valid = guesses_valid?(guesses)
+    end
+
+    p guesses
   end
 
   def play
     set_code
-    puts code.join('')
+    guess
   end
 end
